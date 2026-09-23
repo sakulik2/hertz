8be21275979fb619f03008d957a81b963e881105
Kevin Sue <suxvshen@gmail.com>
2026-09-23 20:30:36 +0800

    chore: resolve dependencies from upstream repositories
    Drops the aliyun mirrors in favour of google()/mavenCentral() so builds do not
    depend on a region-specific mirror. mvn.0110.be/releases still serves TarsosDSP.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,5 @@
 pluginManagement {
     repositories {
-        maven("https://maven.aliyun.com/repository/public")
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -12,8 +9,6 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/repository/public")
-        maven("https://maven.aliyun.com/repository/google")
         google()
         mavenCentral()
         maven("https://jitpack.io")
